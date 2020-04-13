226007fbdfbbfbf269228cc904fcaba6debb9eb5
canary <mchemweno27@gmail.com>
2020-04-13 20:41:12 +0300

    static config

--- houses/urls.py
+++ houses/urls.py
@@ -7,6 +7,4 @@ from . import views
 urlpatterns = [
                   path('', views.get_houses),
                   path('create_house', views.create_house)
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]

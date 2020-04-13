fe5b82cabb8d89d090df41d64fcd10ef618674f3
canary <mchemweno27@gmail.com>
2020-04-13 22:19:54 +0300

    DB changes

--- houses/admin.py
+++ houses/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
 
-from .models import House
+from .models import *
 
 
 # Register your models here.
-admin.site.register(House, LeafletGeoAdmin);
+admin.site.register(House, LeafletGeoAdmin)
+admin.site.register(Dstv)
+admin.site.register(Wifi)
+admin.site.register(Rooms)
+admin.site.register(Category)
--- houses/models.py
+++ houses/models.py
@@ -1,11 +1,42 @@
 from django.contrib.gis.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    house_category = models.CharField(max_length=15)
+
+    def __str__(self):
+        return str(self.id) + '. ' + self.house_category
+
+
+class Rooms(models.Model):
+    number_of_rooms = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id) + '. ' + self.number_of_rooms
+
+
+class Wifi(models.Model):
+    wifi_support = models.CharField(max_length=5)
+
+    def __str__(self):
+        return str(self.id) + '. ' + self.wifi_support
+
+
+class Dstv(models.Model):
+    dstv = models.CharField(max_length=5)
+
+    def __str__(self):
+        return str(self.id) + '. ' + self.dstv
 
 
 class House(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
+
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id) + '. ' + self.name

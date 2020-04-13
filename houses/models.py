0f06a1a2fc93e9460389a53d06f2b7394fc37aa7
canary <mchemweno27@gmail.com>
2020-04-13 22:58:06 +0300

    rooms added

--- houses/models.py
+++ houses/models.py
@@ -34,12 +34,12 @@ class Dstv(models.Model):
 
 class House(models.Model):
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    rooms = models.ForeignKey(Room, on_delete=models.CASCADE, default=1)
-    price = models.IntegerField(default=6000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    rooms = models.ForeignKey(Room, on_delete=models.CASCADE)
+    price = models.IntegerField()
     location = models.PointField()
-    wifi = models.ForeignKey(Wifi, on_delete=models.CASCADE, default=2)
-    dstv = models.ForeignKey(Dstv, on_delete=models.CASCADE, default=2)
+    wifi = models.ForeignKey(Wifi, on_delete=models.CASCADE)
+    dstv = models.ForeignKey(Dstv, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id) + '. ' + self.name

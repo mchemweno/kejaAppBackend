5ec706f09ecf8c105f74f3858da3a06d7f7509dc
canary <mchemweno27@gmail.com>
2020-04-14 14:20:29 +0300

    temporary removal of djoser auth

--- houses/urls.py
+++ houses/urls.py
@@ -5,8 +5,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', include('djoser.urls')),
-    path('', include('djoser.urls.authtoken')),
+    # path('', include('djoser.urls')),
+    # path('', include('djoser.urls.authtoken')),
     path('', views.get_houses),
     path('create_house', views.create_house),
     path('get_houses_point', views.get_houses_around_specific_point)

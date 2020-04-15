a9c4eb7b912e5f5950dad22dd0a0a7a842235a3f
canary <mchemweno27@gmail.com>
2020-04-15 20:16:42 +0300

    aws

--- houses/models.py
+++ houses/models.py
@@ -40,7 +40,7 @@ class House(models.Model):
     price = models.IntegerField()
     location = models.PointField()
     amenities = JSONField(default=my_default)
-    images = ArrayField(ImageField(upload_to='media/house_images/%y/%m/%d'), null=True, blank=True)
+    images = ArrayField(ImageField(upload_to='media/house_images/%y/%m/%d'), blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):

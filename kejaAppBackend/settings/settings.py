1c1ced42e96f74cddd5ab5280dddab4f18461be8
canary <mchemweno27@gmail.com>
2020-05-28 13:25:29 +0300

    os environ

--- kejaAppBackend/settings/settings.py
+++ kejaAppBackend/settings/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# For gmail
+from .gmail_keys import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
